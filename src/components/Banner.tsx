9f0e443b89a91354744b98f99f4d6993042a9076
The-Aaditya-Coder <aadityakaranjekar@gmail.com>
2025-08-21 11:34:28 +0530

    video load poster

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,12 +5,11 @@ const Banner = () => {
   return (
     <section className="banner">
         <div className="img">
-          <ParallaxImg src="/perfumes/1.png" alt="" />
+          <ParallaxImg src="/perfumes/1.jpg" className='blur-[2px]' alt="" />
         </div>
         <div className="banner-copy">
-          <p>Follow Us On Instagram</p>
+          <p>To know more about our products</p>
           <h2 className='text-4xl italic my-2 text-[#faebd7] font-bold'>@sya.perfumes</h2>
-<p>To know more about our products</p>
           <button className="bg-[#faebd7]" onClick={() => window.open('https://www.instagram.com/sya.perfumes/', '_blank')}>Follow Now</button>
         </div>
       </section>
